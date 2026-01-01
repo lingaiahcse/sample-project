6b479da697196158b7b491d49c00aa622a6f9a18
lingaiahcse <blingaiah.iiitbasar@gmail.com>
2026-01-01 16:28:04 +0530

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+      <h1>Hello World</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
