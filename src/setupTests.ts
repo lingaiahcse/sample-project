ebbed5cb665a8cd79d4df9d6ecc58f2332c2e3a0
lingaiahcse <blingaiah.iiitbasar@gmail.com>
2026-01-01 16:47:10 +0530

    testing added

---
+++ src/setupTests.ts
@@ -0,0 +1,10 @@
+import { expect, afterEach, beforeEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+
+beforeEach(() => {
+  // Setup code before each test
+});
+
+afterEach(() => {
+  cleanup();
+});
