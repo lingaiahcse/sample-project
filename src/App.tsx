25d9d145338c1efe0f417c06b1195a3bec94d05a
lingaiahcse <blingaiah.iiitbasar@gmail.com>
2026-01-01 17:47:32 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
-      <h1>Hello World</h1>
+      <h1>Hello Banti</h1>
     </div>
     </>
   )
