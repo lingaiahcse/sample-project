5040dc941233a42bfba65fec515b443d06cafe98
lingaiahcse <blingaiah.iiitbasar@gmail.com>
2026-01-01 17:52:01 +0530

    updated

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,8 +4,8 @@ import App from './App';
 import '@testing-library/jest-dom';
 
 describe('App', () => {
-  it('renders hello banti', () => {
+  it('renders hello bittu', () => {
     render(<App />);
-    expect(screen.getByText(/hello banti/i)).toBeInTheDocument();
+    expect(screen.getByText(/hello bittu/i)).toBeInTheDocument();
   });
 });
